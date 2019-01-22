2a2196f04a2e80a41a4f609a5a43afa3c9dc66e1
zeph <zephamwando@gmail.com>
2019-01-22 15:20:15 +0300

    Initial commit

---
+++ app/src/main/java/com/mwandojrtechnologies/the_smart_parent/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mwandojrtechnologies.the_smart_parent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
